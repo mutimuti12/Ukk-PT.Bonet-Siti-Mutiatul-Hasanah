158f4cba166928b0c82cc5a20e23caa2498401bf
ipp <aippudin600@gmail.com>
2024-02-27 12:48:44 +0700

    ukk bonet 2024

---
+++ resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://instagram.com/Siti_Mutiatul_Hasanah" target="_blank">Siti_Mutiatul_Hasanah</a>.
+</footer>
---
+++ web-gallery-final/resources/views/admin/partials/_footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center text-muted">
+    All Rights Reserved by SMK Al Hafidz Leuwiliang. Designed and Developed by <a href="https://instagram.com/aldi.mds" target="_blank">Aldi Mahardiansyah</a>.
+</footer>
